bfde366a204fd50f667665be064c3178c0f50a66
Gabriel Trindade <gabriel@trindade.dev>
2025-03-05 18:34:46 -0300

    feat: add initial migrations file & db compose file

---
+++ migrations/25-02-05T13_04.sql
@@ -0,0 +1,45 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+
+CREATE TABLE "users" (
+	"id" UUID NOT NULL DEFAULT uuid_generate_v4() UNIQUE,
+	"hash" TEXT NOT NULL,
+	"email" TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "cars" (
+	"id" UUID NOT NULL DEFAULT uuid_generate_v4() UNIQUE,
+	"name" TEXT,
+	"model" TEXT NOT NULL,
+	"year" INTEGER NOT NULL,
+	"brand" TEXT NOT NULL,
+	"color" TEXT NOT NULL,
+	"plate" TEXT NOT NULL,
+	"renavam" TEXT,
+	"aquired_year" INTEGER NOT NULL,
+	"user_id" UUID NOT NULL,
+	PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "entries" (
+	"id" SERIAL NOT NULL UNIQUE,
+	"description" TEXT NOT NULL,
+	"amount" DECIMAL NOT NULL,
+	"odometer" INTEGER,
+	"date" DATE NOT NULL,
+	"place" TEXT,
+	"tags" TEXT,
+	"car_id" UUID NOT NULL,
+	PRIMARY KEY("id")
+);
+
+
+ALTER TABLE "entries"
+ADD FOREIGN KEY("car_id") REFERENCES "cars"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE "cars"
+ADD FOREIGN KEY("user_id") REFERENCES "users"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
