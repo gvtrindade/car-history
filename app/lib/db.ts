7e198b0d17c230b2eb2700f2ae445b759202e6e7
Gabriel Trindade <gabriel@trindade.dev>
2025-03-24 21:06:02 -0300

    refactor: add port to db setup

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,11 +1,11 @@
 import postgres from "postgres";
 
 const sql = postgres({
-  host: process.env.POSTGRES_HOST,
-  port: 5433,
-  database: process.env.POSTGRES_DATABASE,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST ?? "localhost",
+  port: parseInt(process.env.POSTGRES_PORT ?? "5433"),
+  database: process.env.POSTGRES_DATABASE ?? "car-history",
+  username: process.env.POSTGRES_USERNAME ?? "postgres",
+  password: process.env.POSTGRES_PASSWORD ?? "postgres",
 });
 
 export default sql;
